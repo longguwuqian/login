94498dc4e28f9fb0a5639d9500145b1a42aef2f7
longguwuqian <longguwuqian@gmail.com>
2012-07-06 18:06:06 +0800

    。。。。。。。。。。。。。。

--- camera_controller.cpp
+++ camera_controller.cpp
@@ -1,19 +1,16 @@
 #include "camera_controller.h"
-
-// Qt header files
 #include <QtGui>
 
 camera_controller::camera_controller(int device_num)
 {
     cpt_thread = new capture_thread(device_num);
     this->dev_num = device_num;
-} // Controller constructor
+}
 
 camera_controller::~camera_controller()
 {
-    // Delete image buffer
     cpt_thread->disconnect_camera();
-} // Controller destructor
+}
 
 void camera_controller::connect_camera()
 {
@@ -23,7 +20,7 @@ void camera_controller::connect_camera()
 void camera_controller::disconnect_camera()
 {
     cpt_thread->disconnect_camera();
-} // disconnectCamera()
+}
 
 bool camera_controller::is_camera_connected()
 {
@@ -39,26 +36,24 @@ void camera_controller::stop_capture_thread()
 {
     qDebug() << "About to stop capture thread...";
     cpt_thread->stop_capture_thread();
-    // Take one frame off a FULL queue to allow the capture thread to finish
     cpt_thread->wait();
     qDebug() << "Capture thread successfully stopped.";
-} // stopCaptureThread()
+}
 
 void camera_controller::delete_capture_thread()
 {
-    // Delete thread
     delete cpt_thread;
-} // deleteCaptureThread()
+}
 
 int camera_controller::get_input_source_width()
 {
     return cpt_thread->get_input_source_width();
-} // getInputSourceWidth()
+}
 
 int camera_controller::get_input_source_height()
 {
     return cpt_thread->get_input_source_height();
-} // getInputSourceHeight()
+}
 
 
 
--- capture_thread.cpp
+++ capture_thread.cpp
@@ -1,17 +1,11 @@
 #include "capture_thread.h"
-
-// Qt header files
 #include <QDebug>
 
 capture_thread::capture_thread(int device_num):QThread()
 {
-    // Open camera
     capture=cvCaptureFromCAM(device_num);
-    // Initialize variables
     stopped=false;
-} // CaptureThread constructor
-
-
+}
 
 void capture_thread::cvimage2qimage(const IplImage *cvimage, QImage &qimage)
 {
@@ -19,15 +13,11 @@ void capture_thread::cvimage2qimage(const IplImage *cvimage, QImage &qimage)
     if (!cvimage)
             return;
     qimage = QImage(QSize(cvimage->width, cvimage->height), QImage::Format_RGB888);
-            // IplImage * to work with OpenCV functions
     _cvimage = cvCreateImageHeader(cvSize(cvimage->width, cvimage->height), 8, 3);
-            // Share the buffer between QImage and IplImage *
     _cvimage->imageData = (char *)qimage.bits();
     cvCopy(cvimage, _cvimage, 0);
-        // Convert it from BGR to RGB. QImage works with RGB and cvQueryFrame returns a BGR IplImage
     cvCvtColor(_cvimage, _cvimage, CV_BGR2RGB);
 }
-
 
 void capture_thread::run()
 {
@@ -41,13 +31,11 @@ void capture_thread::run()
             break;
         }
         stopped_mutex.unlock();
-        // Capture and add frame to buffer
         this->cvimage2qimage(cvQueryFrame(capture), this->img_frame);
         emit new_frame(&this->img_frame);
-        // Update statistics
     }
     qDebug() << "Stopping capture thread...";
-} // run()
+}
 
 QImage &capture_thread::get_frame()
 {
@@ -65,7 +53,6 @@ void capture_thread::connect_camera(int device_num)
 
 void capture_thread::disconnect_camera()
 {
-    // Disconnect camera if connected
     if (!this->stopped) this->stop_capture_thread();
     if (capture!=NULL)
     {
@@ -75,16 +62,14 @@ void capture_thread::disconnect_camera()
         else
             qDebug() << "ERROR: Camera could not be disconnected.";
     }
-} // disconnectCamera()
-
+}
 
 void capture_thread::stop_capture_thread()
 {
     stopped_mutex.lock();
     stopped=true;
     stopped_mutex.unlock();
-} // stopCaptureThread()
-
+}
 
 bool capture_thread::is_camera_connected()
 {
@@ -92,14 +77,14 @@ bool capture_thread::is_camera_connected()
         return true;
     else
         return false;
-} // isCameraConnected()
+}
 
 int capture_thread::get_input_source_width()
 {
     return cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
-} // getInputSourceWidth()
+}
 
 int capture_thread::get_input_source_height()
 {
     return cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
-} // getInputSourceHeight()
+}
