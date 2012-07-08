cee97c2bedeb744f16e81bbdbe2a9e784ca095cf
longguwuqian <longguwuqian@gmail.com>
2012-07-08 12:59:40 +0800

    bfdjwsfwlj

--- main.cpp
+++ main.cpp
@@ -17,9 +17,12 @@ int main(int argc, char *argv[])
     }
 
     /*设置语言*/
-    QTranslator *translator = new QTranslator(0);
-    if (translator->load("i18n_zh_CN.qm", "./translations") == false) printf("aaaa");
-    a.installTranslator(translator);
+    QTranslator *translator1 = new QTranslator(0);
+    QTranslator *translator2 = new QTranslator(0);
+    translator1->load("i18n_zh_CN.qm", "./translations");
+    translator2->load("qt_zh_CN.qm", "./translations");
+    a.installTranslator(translator1);
+    a.installTranslator(translator2);
 
     login_widget w;
     w.setStyleSheet("QWidget {background-color: beige;}QLabel, QAbstractButton {font: bold;}QPushButton {background-color: palegoldenrod; border-width: 2px; border-color: darkkhaki; border-style: solid; border-radius: 5; padding: 3px; min-width: 9ex; min-height: 2.5ex; } QPushButton:hover {background-color: khaki; }QPushButton:pressed { padding-left: 5px; padding-top: 5px; background-color: #d0d67c;}");
